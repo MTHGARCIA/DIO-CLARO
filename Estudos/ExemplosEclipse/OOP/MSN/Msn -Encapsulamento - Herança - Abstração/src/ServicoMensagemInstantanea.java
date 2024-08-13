08081829a3792c53b07ce91d8628c0e7bb6c24b0
MTHGARCIA <mthgarcia012@gmail.com>
2024-08-12 21:13:15 -0300

    Organizando exemplos MSN

---
+++ Estudos/ExemplosEclipse/OOP/MSN/Msn -Encapsulamento - Herança - Abstração/src/ServicoMensagemInstantanea.java
@@ -0,0 +1,25 @@
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();
+
+}
+
+
+
+    /*public void enviarMensagem() {
+		validarConectadoInternet();
+		
+		System.out.println("Enviando mensagem");
+		
+		salvarHistoricoMensagem();
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando Histórico da Mensagem");
+	}*/
+
